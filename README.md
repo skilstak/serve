a458bd9274afb53b6a648610d595472e9eaec76c
Rob Muhlestein <rob@skilstak.com>
2015-12-12 13:23:08 -0500

    save

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# serve
-Minimal Web Server (HTTP) in Go (with binaries)
+# Go Web Server
+
+This is minimal web server written in Go to 1) serve as an example
+of a basic web API and web file server 2) provide boilerplate for
+very basic microservices 3) add to any web projects that require a
+minimal web server on any platform (such as Phaser.io game development).
+Go's single, runnable files make it ideal for this usage as a minimal
+web server that will run on anything without additional installation
+or configuration. Just download the binary for your platform and
+run it.  Here's how we made it and what it does step by step. (We
+won't get into the details of how Hyper Text Transfer Protocol works
+for now).
+
+## Install
+
+There is no installation step. Just download the binary file from the
+directory matching your platform and run it.
+
+## Usage
+
+Really, really basic, by design:
+
+  serve
+  serve www
+  serve 8080
+  serve www 8080
+  serve 8080 www
+
--- serve.go
+++ serve.go
@@ -1,12 +1,100 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+)
 
+const USAGE = `usage:
+  serve
+  serve www
+  serve 8080
+  serve www 8080
+  serve 8080 www
+`
+
+const SKILSTAK = `
+                 Hello there from ...
+        __   .__.__            __          __          
+  _____|  | _|__|  |   _______/  |______  |  | __      
+ /  ___/  |/ /  |  |  /  ___/\   __\__  \ |  |/ /      
+ \___ \|    <|  |  |__\___ \  |  |  / __ \|    <       
+/____  >__|_ \__|____/____  > |__| (____  /__|_ \______
+     \/     \/            \/            \/     \/_____/
+               Coding Arts
+`
+
+var ldir string
+var port string = ":8080"
+var dir string = "www"
+
+// Prints usage to stderr
+func usage() {
+	fmt.Fprint(os.Stderr, USAGE)
+}
+
+// Writes text directly to the browser
+func skilstakHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, SKILSTAK)
+}
+
+// Sends files to the browser
 func serve(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "www/"+r.URL.Path[1:])
+	file := r.URL.Path
+	http.ServeFile(w, r, dir+"/"+file)
+	log.Print(r.Method, " ", file)
+
+}
+
+func parseArgs() {
+	argc := len(os.Args)
+	switch {
+	case argc == 1:
+		return
+	case argc == 2:
+		a := os.Args[1]
+		if a == "help" || a == "-h" || a == "/h" {
+			usage()
+			os.Exit(0)
+		}
+		n, _ := strconv.Atoi(a)
+		if n > 0 {
+			port = ":" + a
+		} else {
+			dir = a
+		}
+	case argc == 3:
+		a := os.Args[1]
+		b := os.Args[2]
+		n, _ := strconv.Atoi(a)
+		if n > 0 {
+			port = ":" + a
+			dir = b
+		} else {
+			port = ":" + b
+			dir = a
+		}
+	}
 }
 
 func main() {
+	wd, _ := os.Getwd()
+	parseArgs()
+	ldir = filepath.Join(wd, dir)
+
+	// first match wins
+	http.HandleFunc("/skilstak", skilstakHello)
 	http.HandleFunc("/", serve)
-	http.ListenAndServe(":8001", nil)
+
+	log.Printf("SERVE %s on %s", ldir, port)
+
+	// loops infinitely listening and serving until interrupted
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
